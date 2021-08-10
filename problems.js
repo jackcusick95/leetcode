a62462c5f8c47c67a083bc28f0c7509259a2c91d
jackcusick95 <jackcusick95@gmail.com>
2021-08-10 10:59:56 -0400

    more code

--- problems.js
+++ problems.js
@@ -28,3 +28,43 @@ var isPalindrome = function (x) {
   let revX = parseInt(x.toString().split("").reverse().join(""));
   if (revX == x) return true;
 };
+
+
+// Given a roman numeral, convert it to an integer.
+
+var romanToInt = function(s) {
+	const roman = {
+	    "I": 1,
+	    "V": 5,
+	    "X": 10,
+	    "L": 50,
+	    "C": 100,
+	    "D": 500,
+	    "M": 1000
+	}
+	
+	let ans = 0
+	for (let i = s.length-1; i >= 0; i--) {
+	    let num = roman[s.charAt(i)]
+	    if (4 * num < ans) ans -= num
+	    else ans += num
+	}
+	return ans
+    };
+
+//     Write a function to find the longest common prefix string amongst an array of strings.
+
+//     If there is no common prefix, return an empty string "".
+    
+    var longestCommonPrefix = function(strs) {
+	if (strs == null || strs.length == 0) return "";
+	for (let i = 0; i < strs[0].length; i++) {
+	    let char = strs[0].charAt(i);
+	    for (let j = 1; j < strs.length; j++) {
+		if (i == strs[j].length || strs[j].charAt(i) != char)
+		    return strs[0].slice(0, i)
+	    }
+	}
+	return strs[0]; 
+    };
+    
